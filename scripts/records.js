645f801798e388da5868dd47a8b366da2cc13cac
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-28 17:21:34 -0700

    implementing sort records functionality, not done

--- scripts/records.js
+++ scripts/records.js
@@ -61,34 +61,131 @@ function deleteRecord(id) {
     }
 }
 
-$(document).ready(function () {
-    showRecords()
-    $("#filter").click(function () {
-        var search_term = $("#search-record-bar").val()
-        var field = $("#field").val()
-        // search_term = "%" + search_term + "%"
-        $("#recordCardGroup").empty()
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                var userID = user.uid;            
-                let recordTemplate = document.getElementById("recordTemplate");
-                let recordCardGroup = document.getElementById("recordCardGroup");
-                
+function sort_asce_desc(category, filter_type) {
+    $("#recordCardGroup").empty()
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            //go to the correct user document by referencing to the user uid
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;            
+            //get the document for current user.
+            let recordTemplate = document.getElementById("recordTemplate");
+            let recordCardGroup = document.getElementById("recordCardGroup");
+            if (filter_type == "alpha") {
                 db.collection("records")
                 .where("userID", "==", userID)
-                .where(field, "==", search_term)
+                .orderBy(category)
+                .onSnapshot(allRecords => {
+                    $("#recordCardGroup").empty()
+                    records = allRecords.docs;
+                    records.forEach(doc => {
+                        var docID = doc.id
+                        var name = doc.data().name; //gets the name field
+                        var type = doc.data().type; //gets the unique ID field
+                        var brand = doc.data().brand;
+                        var cost = doc.data().cost; //gets the length field
+                        var time = doc.data().timestamp.toDate().toDateString();
+                        
+                        let recordCard = recordTemplate.content.cloneNode(true);
+                        recordCard.querySelector('#item_name').innerHTML = name;     //equiv getElementByClassName
+                        recordCard.querySelector('#date_added').innerHTML = time;    //equiv getElementByClassName
+                        recordCard.querySelector('#item_type').innerHTML = type;
+                        recordCard.querySelector('#item_brand').innerHTML = brand;
+                        recordCard.querySelector('#item_cost').innerHTML = cost;  //equiv getElementByClassName
+                        recordCard.querySelector('.edit-record').setAttribute("id", docID)
+                        recordCard.querySelector('.delete-record').setAttribute("id", docID)
+                        recordCardGroup.appendChild(recordCard);
+                    })
+                })
+            }
+            else {
+                db.collection("records")
+                .where("userID", "==", userID)
+                .orderBy(category, "desc")
+                .onSnapshot(allRecords => {
+                    $("#recordCardGroup").empty()
+                    records = allRecords.docs;
+                    records.forEach(doc => {
+                        var docID = doc.id
+                        var name = doc.data().name; //gets the name field
+                        var type = doc.data().type; //gets the unique ID field
+                        var brand = doc.data().brand;
+                        var cost = doc.data().cost; //gets the length field
+                        var time = doc.data().timestamp.toDate().toDateString();
+                        
+                        let recordCard = recordTemplate.content.cloneNode(true);
+                        recordCard.querySelector('#item_name').innerHTML = name;     //equiv getElementByClassName
+                        recordCard.querySelector('#date_added').innerHTML = time;    //equiv getElementByClassName
+                        recordCard.querySelector('#item_type').innerHTML = type;
+                        recordCard.querySelector('#item_brand').innerHTML = brand;
+                        recordCard.querySelector('#item_cost').innerHTML = cost;  //equiv getElementByClassName
+                        recordCard.querySelector('.edit-record').setAttribute("id", docID)
+                        recordCard.querySelector('.delete-record').setAttribute("id", docID)
+                        recordCardGroup.appendChild(recordCard);
+                    })
+                })
+            }
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });    
+}
+
+$(document).ready(function () {
+    showRecords()
+    $("#sort").click(function () {
+        var field = $("#field").val()
+        var sortby = $(".sort").val()
+        const alphabetical_fields = ["name", "type", "brand", "serial_num", "cost"]
+        console.log(field)
+        console.log(sortby)
+        console.log(sortby == "alpha")
+        console.log(alphabetical_fields.indexOf(field) != -1)
+        if ((sortby == "alpha" || sortby == "reverse-alpha") && alphabetical_fields.indexOf(field) != -1) {
+            sort_asce_desc(field, sortby)
+        }
+        else if (sortby == "damaged" || sortby == "not-damaged") {
+            console.log("sorting by damage")
+        }
+        else {
+            console.log("no sort")
+        }
+    })
+    $("#search").click(function () {
+        var search_term = $("#search-record-bar").val()
+        var field = $("#field").val()
+        console.log(field)
+        if (search_term == "") {
+            alert("Please enter in search term!")
+        }
+        else if (field == "Category") {
+            alert("Please select a category!")
+        }
+        else {
+            $("#recordCardGroup").empty()
+            firebase.auth().onAuthStateChanged(user => {
+                if (user) {
+                    var userID = user.uid;            
+                    let recordTemplate = document.getElementById("recordTemplate");
+                    let recordCardGroup = document.getElementById("recordCardGroup");
+                    
+                    db.collection("records")
+                    .where("userID", "==", userID)
+                    .where(field, "==", search_term)
                     .onSnapshot(allRecords => {
                         records = allRecords.docs;
                         console.log(records);
                         records.forEach(doc => {
-
+                            
                             var docID = doc.id;
                             var name = doc.data().name; //gets the name field
                             var type = doc.data().type; //gets the unique ID field
                             var brand = doc.data().brand;
                             var cost = doc.data().cost; //gets the length field
                             var time = doc.data().timestamp.toDate().toDateString();
-
+                            
                             let recordCard = recordTemplate.content.cloneNode(true);
                             recordCard.querySelector('#item_name').innerHTML = name;     //equiv getElementByClassName
                             recordCard.querySelector('#date_added').innerHTML = time;    //equiv getElementByClassName
@@ -98,12 +195,13 @@ $(document).ready(function () {
                             recordCardGroup.appendChild(recordCard);
                         })
                     })
-            } 
-            else {
-                // No user is signed in.
-                console.log ("No user is signed in");
-            }
-        });    
+                } 
+                else {
+                    // No user is signed in.
+                    console.log ("No user is signed in");
+                }
+            });    
+        }
 
     })
 
