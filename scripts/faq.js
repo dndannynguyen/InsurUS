c8f47a823025c2cd79db5591f3880a83d743169f
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-04-02 13:27:54 -0700

    added faq writing to the database functionality

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,40 +1,17 @@
-var currentUser
+function writeQuestion() {
+    // writes the user's question to the database
+    let userEmail = $("#Email").val();
+    let Question = $("#Question").val();
+    db.collection("faq").add({
+        email: userEmail,
+        question: Question,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(() => {
+        swal("Your question has been recorded and will be answered within 5 business days!")
+    })
 
-function saveHikeDocumentIDAndRedirect(){
-    let params = new URL(window.location.href) //get the url from the search bar
-    let ID = params.searchParams.get("docID");
-    localStorage.setItem('hikeDocID', ID);
-    window.location.href = 'FAQ.html';
 }
 
-function writeFaq() {
-    console.log("inside write FAQ")
-    let userEmail = document.getElementById("Email").value;
-    let message = document.getElementById("Message").value;
-    console.log(Email, Message);
-
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var currentUser = db.collection("users").doc(user.uid)
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("faq").add({
-                        faqDocID: faqDocID,
-                        email: Email,
-                        message: Message,
-                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                    }).then(() => {
-                        window.location.href = "FAQ.html"; //new line added
-                    })
-                })
-        } else {
-            window.location.href = 'FAQ.html';
-        }
-    });
-}
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
+$(document).ready(
+    $("#submitBTN").click(writeQuestion)
+)
