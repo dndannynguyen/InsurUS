e6828f7cd42eb2beddda2814ce5a7ccab3d27d0c
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-04-03 22:34:30 -0700

    styling and main page

--- scripts/main.js
+++ scripts/main.js
@@ -18,29 +18,6 @@ function insertName() {
     });
 }
 
-insertName(); //run the function
-
-//-----------------------------------------------
-// Create a "max" number of objects
-//-----------------------------------------------
-// function addObjects(max_objects) {
-
-//     var itemsRef = db.collection("stored_items");
-//     for (i = 1; i <= max_objects; i++) {
-//         itemsRef.add({
-//             name: "item number " + i,
-//             details: "This is an item the user added",
-//             lat: 49+i,
-//             lng: -122+i, 
-//             last_updated: firebase.firestore.FieldValue.serverTimestamp()
-//         })
-//     }
-// }
-
-
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
 
 function displayRecordsDynamically(collection) {
     let cardTemplate = document.getElementById("Template");
@@ -51,42 +28,117 @@ function displayRecordsDynamically(collection) {
             .where("userID", "==", userID)
             .orderBy("timestamp", "desc")
             .limit(3)
-                .onSnapshot(allRecords=> {
+            .onSnapshot(allRecords=> {
+                
+                allRecords.forEach(doc => { //iterate thru each doc
+                    var type = doc.data().type;
+                    var name = doc.data().name;
+                    var cost = doc.data().cost;
+                    var brand = doc.data().brand;
+                    var docID = doc.id;
+                    let newcard = cardTemplate.content.cloneNode(true);
                     
-                    allRecords.forEach(doc => { //iterate thru each doc
-                        var type = doc.data().type;
-                        var name = doc.data().name;
-                        var cost = doc.data().cost;
-                        var brand = doc.data().brand;
-                        var docID = doc.id;
-                        let newcard = cardTemplate.content.cloneNode(true);
-        
-        
-                        //update title and text and image
-                        newcard.querySelector('.card-title').innerHTML = name;
-                        newcard.querySelector('.card-type').innerHTML = "Type: " + type;
-                        newcard.querySelector('.card-cost').innerHTML = "Cost: " + cost;
-                        newcard.querySelector('.card-brand').innerHTML = "Brand: " + brand;
-                        newcard.querySelector('a').href = "eachItem.html?docID="+docID;
-                        newcard.querySelector('.delete-record').setAttribute("id", docID)
-                        //attach to gallery
-                        document.getElementById(collection + "-go-here").appendChild(newcard);
-        
-                    })
+                    
+                    //update title and text and image
+                    newcard.querySelector('.card-title').innerHTML = name;
+                    newcard.querySelector('.card-type').innerHTML = "Type: " + type;
+                    newcard.querySelector('.card-cost').innerHTML = "Cost: " + cost;
+                    newcard.querySelector('.card-brand').innerHTML = "Brand: " + brand;
+                    newcard.querySelector('a').href = "eachItem.html?docID="+docID;
+                    newcard.querySelector('.delete-record').setAttribute("id", docID)
+                    //attach to gallery
+                    document.getElementById(collection + "-go-here").appendChild(newcard);
+                    
                 })
+            })
         }
     })
 }
 
-displayRecordsDynamically("records");
-  
+
 function deleteRecord(id) {
     swal("Are you sure you want to delete this record?", {
         buttons: [true, "Delete"]
     })
     $(".swal-button--confirm").click(function () {
         db.collection("records").doc(id).delete().then(() => {
-                    window.location.href = "main.html"; //new line added
-                })
+            window.location.href = "main.html"; //new line added
+        })
     })
 }
+
+function displayProvidersDynamically(collection) {
+    let cardTemplate = document.getElementById("providerCardTemplate");
+
+    db.collection(collection)
+        .limit(3)
+        .get() //the collection called "hikes"
+        .then(allProviders => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allProviders.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name; // get value of the "name" key
+                var details = doc.data().details; // get value of the "details" key
+                var providerCode = doc.data().code; //get unique ID to each provider to be used for fetching right image
+                var rating = doc.data().rating; //gets the rating field
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image etc.
+                newcard.querySelector('.card-title').innerHTML = title;
+                //newcard.querySelector('.card-length').innerHTML = providerLength + "km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${providerCode}.png`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "profile.html?docID=" + docID;
+
+                //NEW LINE: update to display length, duration, last updated
+                newcard.querySelector('.card-length').innerHTML =
+                    "Rating: " + doc.data().rating + " <br>" +
+                    "Last updated: " + doc.data().last_updated.toDate().toLocaleDateString();
+
+                //NEW LINES: next 2 lines are new for demo#11
+                //this line sets the id attribute for the <i> tag in the format of "save-hikdID" 
+                //so later we know which provider to bookmark based on which provider was clicked
+                newcard.querySelector('i').id = 'save-' + docID;
+                // this line will call a function to save the providers to the user's document             
+                newcard.querySelector('i').onclick = () => saveBookmark(docID);
+
+
+                newcard.querySelector('b').id = 'register-' + docID;
+                                // this line will call a function to save the providers to the user's document             
+                newcard.querySelector('b').onclick = () => saveRegister(docID);
+
+
+                currentUser.get().then(userDoc => {
+                    //get the user name
+                    var bookmarks = userDoc.data().bookmarks;
+                    if (bookmarks.includes(docID)) {
+                       document.getElementById('save-' + docID).innerText = 'favorite';
+                    }
+                    // var registration = userDoc.data().registration;
+                    // if (registration.includes(docID)) {
+                    // document.getElementById('register-' + docID).innerText = 'register';
+                    // }
+                })
+                
+
+                
+                // currentUser.get().then(userDoc => {
+                //     //get the user name
+                   
+                // })
+
+
+                //Finally done modifying newcard
+                //attach to gallery, Example: "providers-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+$(document).ready(function () {
+    insertName() //run the function
+    displayRecordsDynamically("records")
+    displayProvidersDynamically("providers")
+})
