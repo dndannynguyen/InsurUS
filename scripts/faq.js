42d71b11cec86824463da366cd30a64fd7ca9aed
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-04-04 17:23:24 -0700

    deployed site in firebase

--- scripts/faq.js
+++ scripts/faq.js
@@ -10,49 +10,8 @@ function writeQuestion() {
         swal("Your question has been recorded and will be answered within 5 business days!")
     })
 
-<<<<<<< HEAD
-function savefaqDocumentIDAndRedirect(){
-    let params = new URL(window.location.href) //get the url from the search bar
-    let ID = params.searchParams.get("docID");
-    localStorage.setItem('faqDocID', ID);
-    window.location.href = 'FAQ.html';
-}
-
-function writeFaq() {
-    console.log("inside write FAQ")
-    let userEmail = document.getElementById("email").value;
-    let message = document.getElementById("Message").value;
-    console.log(Email, Message);
-
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var currentUser = db.collection("users").doc(user.uid)
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("faq").add({
-                        faqDocID: faqDocID,
-                        email: Email,
-                        message: Message,
-                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                    }).then(() => {
-                        window.location.href = "FAQ.html"; //new line added
-                    })
-                })
-        } else {
-            window.location.href = 'FAQ.html';
-        }
-    });
-}
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
-=======
 }
 
 $(document).ready(
     $("#submitBTN").click(writeQuestion)
 )
->>>>>>> eba929cbe0bcbf03016e622667e67ba30717a824
